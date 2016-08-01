a1d5723515618fe9b066d13dfce1088edbda3d26
silo4 <329656527@qq.com>
2016-08-01 23:57:04 +0800

    modify variable name

--- app/src/main/java/dont/be/shy/ui/av/VideoSettingActivity.java
+++ app/src/main/java/dont/be/shy/ui/av/VideoSettingActivity.java
@@ -31,7 +31,7 @@ public class VideoSettingActivity extends BaseActivity implements SurfaceHolder.
     private final static String TAG = VideoSettingActivity.class.getSimpleName();
 
     @BindView(R.id.svVideo)
-    SurfaceView mSurfaceview;  // SurfaceView对象：(视图组件)视频显示
+    SurfaceView mSurfaceView;  // SurfaceView对象：(视图组件)视频显示
 
     private SurfaceHolder mSurfaceHolder = null;  // SurfaceHolder对象：(抽象接口)SurfaceView支持类
     private Camera mCamera = null;     // Camera对象，相机预览
@@ -50,7 +50,7 @@ public class VideoSettingActivity extends BaseActivity implements SurfaceHolder.
     }
 
     private void initSurfaceView(){
-        mSurfaceHolder = mSurfaceview.getHolder();
+        mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.addCallback(this);
     }
 
